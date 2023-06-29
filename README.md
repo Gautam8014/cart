3dc0110a6c3cb7ee95e9c2a2ff1c1e1a22732b44
Gautam8014 <flt.gautam@gmail.com>
2023-06-29 21:29:46 +0530

    add

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# cart
+# AddToCartt
+
+<img align="right" alt="Coding" width="100%" src="/Screenshot%20(939).png">
+
+<h1>Add to Cart Mini Project</h1>
+
+
+The Add to Cart mini project is a simple implementation of an e-commerce shopping cart feature using HTML, CSS, and JavaScript. It provides a user-friendly interface for users to add items to their cart and view the selected items.
+
+<h3>Features</h3>
+
+<i>Product Listing</i>: The project displays a list of products with their names, prices, and Add to Cart buttons.
+
+<i>Add to Cart</i>: Users can add products to their cart by clicking on the Add to Cart button.
+
+<i>Cart Summary</i>: The project maintains a summary of the items added to the cart, including the total number of items and the total cost.
+
+<i>Responsive Design</i>: The layout adapts to different screen sizes, ensuring a seamless experience on desktop and mobile devices.
+
+Designed and developed with ❤️ by Minal Kumari
---
+++ script.js
@@ -0,0 +1,108 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Scrambled Egg with Spegatties',
+        image: '1.PNG',
+        price: 1800
+    },
+    {
+        id: 2,
+        name: 'Chicken Wings',
+        image: '2.PNG',
+        price: 2000
+    },
+    {
+        id: 3,
+        name: 'Loads of Chicken Salad',
+        image: '3.PNG',
+        price: 1900
+    },
+    {
+        id: 4,
+        name: 'Mango Pudding',
+        image: '4.PNG',
+        price: 1000
+    },
+    {
+        id: 5,
+        name: 'Fresh Salad',
+        image: '5.PNG',
+        price: 1400
+    },
+    {
+        id: 6,
+        name: 'Tomato Pizza',
+        image: '6.PNG',
+        price: 1500
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
